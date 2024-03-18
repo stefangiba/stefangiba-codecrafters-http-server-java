6963cd4b5b136d947cf6928b4aaa1659bc009b1a
Stefan Giba <stefang867@gmail.com>
2024-03-18 07:48:26 +0000

    responding with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
+  private static final String HTTP_OK = "HTTP/1.1 200 OK\r\n\r\n";
+
   public static void main(String[] args) {
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
@@ -12,8 +18,25 @@ public class Main {
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
+      // handle incoming connection
+      InputStream is = clientSocket.getInputStream();
+      String request = readRequest(is);
+      clientSocket.getOutputStream().write(HTTP_OK.getBytes());
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  private static String readRequest(InputStream is) throws IOException {
+    int bufferSize = 1024;
+    char[] buffer = new char[bufferSize];
+    StringBuilder out = new StringBuilder();
+    Reader in = new InputStreamReader(is, StandardCharsets.UTF_8);
+    
+    for (int numRead; (numRead = in.read(buffer, 0, buffer.length)) > 0;) {
+      out.append(buffer, 0, numRead);
+    }
+
+    return out.toString();
   }
 }
