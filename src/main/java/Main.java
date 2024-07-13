b73c3834ac3cb2ed799b3e5ee0409d4a2c9396fe
Stefan Giba <stefang867@gmail.com>
2024-07-13 21:42:40 +0100

    implement /files/{file_name}

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,19 +3,36 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+  private static final String PATH_ECHO = "/echo";
+  private static final String PATH_USER_AGENT = "/user-agent";
+  private static final String PATH_FILES = "/files";
+
   private static final String HTTP_OK = "HTTP/1.1 200 OK\r\n\r\n";
   private static final String HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
   private static final String HTTP_RESPONSE = "HTTP/1.1 200 OK\r\nContent-type: %s\r\nContent-Length: %d\r\n\r\n%s";
-  private static final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
 
   public static void main(String[] args) throws InterruptedException, IOException {
-    ServerSocket serverSocket = new ServerSocket(4221);
+    if (args.length != 2 || !"--directory".equals(args[0])) {
+      throw new IllegalArgumentException("Usage: --directory <absolute path to directory to retrieve files from>");
+    }
 
-    try {
+    Path directoryPath = Paths.get(args[1]);
+    if (!directoryPath.isAbsolute()) {
+      System.err.println("Error: Directory path must be absolute");
+      throw new IllegalArgumentException("Usage: --directory <absolute path to directory to retrieve files from>");
+    }
+
+    try (ServerSocket serverSocket = new ServerSocket(4221);
+        ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
       while (true) {
         Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
 
@@ -30,9 +47,22 @@ public class Main {
 
             String response = switch (request.getPath()) {
               case "/" -> HTTP_OK;
-              case String path when path.startsWith("/echo") -> buildTextResponse(path.split("/")[2]);
-              case String path when path.startsWith("/user-agent") ->
+              case String path when path.startsWith(PATH_ECHO) -> buildTextResponse(path.split("/")[2]);
+              case String path when path.startsWith(PATH_USER_AGENT) ->
                 buildTextResponse(request.getHeader("User-Agent"));
+              case String path when path.startsWith(PATH_FILES) -> {
+                try {
+                  Path filePath = directoryPath.resolve(path.substring(7));
+                  byte[] fileContent = Files.readAllBytes(filePath);
+
+                  yield buildResponse(new String(fileContent, StandardCharsets.UTF_8), "application/octet-stream",
+                      fileContent.length);
+                } catch (IOException e) {
+                  System.err.println(e.getMessage());
+
+                  yield HTTP_NOT_FOUND;
+                }
+              }
               default -> HTTP_NOT_FOUND;
             };
 
@@ -42,13 +72,14 @@ public class Main {
           }
         });
       }
-    } finally {
-      executorService.shutdown();
-      serverSocket.close();
     }
   }
 
   private static String buildTextResponse(String content) {
-    return String.format(HTTP_RESPONSE, "text/plain", content.length(), content);
+    return buildResponse(content, "text/plain", -1);
+  }
+
+  private static String buildResponse(String content, String contentType, int contentLength) {
+    return String.format(HTTP_RESPONSE, contentType, contentLength == -1 ? content.length() : contentLength, content);
   }
 }
