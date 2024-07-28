0aa935a191a58c58e26dd02a27c5a97c71c3372e
Stefan Giba <stefang867@gmail.com>
2024-07-28 11:18:59 +0100

    allow file creation functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,9 +19,10 @@ public class Main {
   private static final String HTTP_OK = "HTTP/1.1 200 OK\r\n\r\n";
   private static final String HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
   private static final String HTTP_RESPONSE = "HTTP/1.1 200 OK\r\nContent-type: %s\r\nContent-Length: %d\r\n\r\n%s";
+  private static final String HTTP_CREATED = "HTTP/1.1 201 Created\r\n\r\n";
 
   public static void main(String[] args) throws InterruptedException, IOException {
-    Optional<Path> directoryPath = getDirectoryPath(args);
+    Optional<Path> directoryPathOpt = getDirectoryPath(args);
 
     try (ServerSocket serverSocket = new ServerSocket(4221);
         ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
@@ -44,21 +44,18 @@ public class Main {
               case String path when path.startsWith(PATH_USER_AGENT) ->
                 buildTextResponse(request.getHeader("User-Agent"));
               case String path when path.startsWith(PATH_FILES) -> {
-                try {
-                  if (directoryPath.isEmpty()) {
-                    yield HTTP_NOT_FOUND;
-                  }
-
-                  var filePath = directoryPath.get().resolve(path.substring(7));
-                  byte[] fileContent = Files.readAllBytes(filePath);
-
-                  yield buildResponse(new String(fileContent, StandardCharsets.UTF_8), "application/octet-stream",
-                      fileContent.length);
-                } catch (IOException e) {
-                  System.err.println(e.getMessage());
-
+                if (directoryPathOpt.isEmpty()) {
                   yield HTTP_NOT_FOUND;
                 }
+
+                var directoryPath = directoryPathOpt.get();
+
+                yield switch (request.getHttpMethod()) {
+                  case GET -> retrieveFile(directoryPath, path.substring(7));
+                  case POST -> writeToFileString(directoryPath, path.substring(7), request.getBody());
+                  default ->
+                    HTTP_NOT_FOUND;
+                };
               }
               default -> HTTP_NOT_FOUND;
             };
@@ -69,6 +66,33 @@ public class Main {
           }
         });
       }
+    }
+  }
+
+  private static String retrieveFile(Path directoryPath, String filePathStr) {
+    try {
+      var filePath = directoryPath.resolve(filePathStr);
+      byte[] fileContent = Files.readAllBytes(filePath);
+
+      return buildResponse(new String(fileContent, StandardCharsets.UTF_8), "application/octet-stream",
+          fileContent.length);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+
+      return HTTP_NOT_FOUND;
+    }
+  }
+
+  private static String writeToFileString(Path directoryPath, String filePathStr, String content) {
+    try {
+      var filePath = directoryPath.resolve(filePathStr);
+      Files.write(filePath, content.getBytes());
+
+      return HTTP_CREATED;
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+
+      return HTTP_NOT_FOUND;
     }
   }
 
