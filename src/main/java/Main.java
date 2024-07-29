af5ca793135f5f3c5e9182c8886a701ebe789e64
Stefan Giba <stefang867@gmail.com>
2024-07-29 15:50:31 +0100

    initial support for gzip compression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,8 +14,11 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+
     private static final String[] SUPPORTED_ENCODINGS = { "gzip" };
 
     private static final String PATH_ECHO = "/echo";
@@ -76,13 +80,15 @@ public class Main {
             byte[] fileContent = Files.readAllBytes(filePath);
 
             Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Length", Integer.toString(fileContent.length));
             headers.put("Content-Type", "application/octet-stream");
 
             var supportedEncoding = getSupportedEncoding(request);
             if (supportedEncoding.isPresent()) {
                 headers.put("Content-Encoding", supportedEncoding.get());
+                fileContent = zip(fileContent);
             }
+
+            headers.put("Content-Length", Integer.toString(fileContent.length));
 
             return new HttpResponse(HttpVersion.HTTP_1_1, HttpStatusCode.OK, headers,
                     new String(fileContent, StandardCharsets.UTF_8));
@@ -106,18 +112,41 @@ public class Main {
         }
     }
 
-    private static HttpResponse buildTextResponse(String responseContent, HttpRequest request) {
+    private static HttpResponse buildTextResponse(String responseContent, HttpRequest request) throws IOException {
         Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Length", Integer.toString(responseContent.length()));
         headers.put("Content-Type", "text/plain");
 
         var supportedEncoding = getSupportedEncoding(request);
-
         if (supportedEncoding.isPresent()) {
             headers.put("Content-Encoding", supportedEncoding.get());
+            responseContent = bytesToHex(zip(responseContent));
         }
 
+        headers.put("Content-Length", Integer.toString(responseContent.length()));
+
         return new HttpResponse(HttpVersion.HTTP_1_1, HttpStatusCode.OK, headers, responseContent);
+    }
+
+    private static final byte[] zip(String s) throws IOException {
+        return zip(s.getBytes());
+    }
+
+    private static final byte[] zip(byte[] bytes) throws IOException {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream)) {
+            gzipOutputStream.write(bytes);
+            return byteArrayOutputStream.toByteArray();
+        }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
     }
 
     private static Optional<String> getSupportedEncoding(HttpRequest request) {
